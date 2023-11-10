c12c073065ab572d2cb6c5b35fad5af308c986a9
Arion <Arion@DESKTOP-9BU80RS>
2023-11-10 20:49:11 +0200

    Добавьте файлы проекта.

---
+++ Laba34/Program.cs
@@ -0,0 +1,12 @@
+﻿//средний 26
+Console.WriteLine("|    x   |   y    |");
+for (double x = -5; x <= 10; x ++)
+{
+        double y;
+        double k=x-Math.Sin(x);
+        if (x < 0) y = 1 - Math.Pow(x, 2);
+        else if (x >= 0 && x <= 12.5) y = Math.Pow(Math.E,k)+Math.Cos(0.01*x);
+        else y = Math.Pow(x,2)+2.5*Math.Pow(x,2);
+        Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+ 
+}
